f66eae02962f27d2923c7eb646c6ccf7013330b1
Stroom <Sanderstroom@gmail.com>
2012-10-03 00:30:52 -0700

    arvatavasti ei tööta. Vajab testimist.

--- app/api.py
+++ app/api.py
@@ -12,27 +12,29 @@ class Search ():
 class Open ():
     """
     API open entity function.
-    
+    Needs entity ID.
+    TODO MAYBE needs public/non-public???
     """
     def get(self):
-        print "Hello open!";
-                
-        # TODO rest
+        entity = db.Entity(user_locale=self.get_user_locale())
+        
+        self.write(entity.get(entity_id=self.get_argument('id'), limit=1, full_definition=True, only_public=False))
+        self.write("Open: Done.\n")
 class Add (myRequestHandler):
     """
     API add entity function.
-    
+    Needs entity ID and parent ID.
     """
     def get(self):
-        self.write("Hello add!\n");
+        self.write("Add: 1\n");
         
         entity = db.Entity(user_locale=self.get_user_locale())
         
-        self.write("natuke tehtud\n")
+        self.write("Add: 2\n")
         
-        entity_id = entity.create(entity_definition_keyname=self.get_argument('id'))
+        entity_id = entity.create(entity_definition_keyname=self.get_argument('id'), parent_entity_id=('parent_id'))
         
-        self.write("olen tubli\n")
+        self.write("Add: 3. Done.\n")
 class Edit ():
     """
     API edit entity function.
