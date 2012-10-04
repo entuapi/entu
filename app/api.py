61a7804949bcd85d9aefd1f00b5871815052d656
Karl-Oskar Masing <komasing@gmail.com>
2012-10-04 22:09:34 +0300

    A little better "Add" plus added some html error raising.

--- app/api.py
+++ app/api.py
@@ -1,5 +1,6 @@
 from helper import *
 from db import Entity
+from tornado import web
 import json
 
 class Search (myRequestHandler):
@@ -21,7 +22,7 @@ class Search (myRequestHandler):
         limit = self.get_argument('limit',None)
         full_definition = False
         public = True if self.get_argument('public','0') == '1' else False
-       
+        
         entity = db.Entity(user_locale=self.get_user_locale())
         
         result = entity.get(ids_only=ids_only,entity_id=entity_id,search=keywords,
@@ -37,6 +38,9 @@ class Search (myRequestHandler):
             for entity in result:
                 entity_ids.append(entity['id'])
             self.write(json.dumps(entity_ids))
+            
+    def post(self):
+        raise web.HTTPError(405,'POST method not supported.')
  
         
         
@@ -51,21 +55,32 @@ class View (myRequestHandler):
         
         self.write(entity.get(entity_id=self.get_argument('id'), limit=1, full_definition=True, only_public=False))
         self.write("Open: Done.\n")
+        
+    def post(self):
+        raise web.HTTPError(405,'POST method not supported.')
+        
 class Add (myRequestHandler):
     """
     API add entity function.
-    Needs entity ID and parent ID.
+    Needs entity ID and optionally parent ID.
     """
     def get(self):
-        self.write("Add: 1\n");
+        
+        entity_definition = self.get_argument('entity_type',None)
+        
+        if entity_definition == None:
+            raise web.HTTPError(400,'Entity type required.')
+        
+        parent = self.get_argument('parent', None)
         
         entity = db.Entity(user_locale=self.get_user_locale())
+        entity_id = entity.create(entity_definition_keyname=entity_definition,parent_entity_id=parent)
         
-        self.write("Add: 2\n")
         
-        entity_id = entity.create(entity_definition_keyname=self.get_argument('id'), parent_entity_id=('parent_id'))
         
-        self.write("Add: 3. Done.\n")
+    def post(self):
+        raise web.HTTPError(405,'Currently using GET method.')
+        
 class Edit (myRequestHandler):
     """
     API edit entity function.
@@ -74,6 +89,9 @@ class Edit (myRequestHandler):
     def get(self):
         print "Hello edit!";
         #TODO rest
+   
+    def post(self):
+        raise web.HTTPError(405,'Currently using GET method.')
         
         
 handlers = [
