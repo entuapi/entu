b37e76244e2606bb11405bf99de8b8e96288a41a
Karl-Oskar Masing <komasing@gmail.com>
2012-10-04 17:37:06 +0300

    Renamed vague "Open" method name to "View";  implemented "Search" method

--- app/api.py
+++ app/api.py
@@ -1,15 +1,46 @@
 from helper import *
 from db import Entity
+import json
 
-class Search ():
+class Search (myRequestHandler):
     """
-    API search for entities function.
+    API search for entities based on keywords,entity definition, 
+    dataproperty [and entity properties - not implemented].
+    
+    Returns entity ids if there are more than 5 query results or otherwise 
+    respective amount of full entities in json format.
     
     """
     def get(self):
-        print "Hello search!";
-        # TODO rest
-class Open (myRequestHandler):
+        
+        ids_only = False
+        entity_id=None
+        keywords = self.get_argument('keywords',None)
+        entity_definition = self.get_argument('entity_type', None)
+        dataproperty = self.get_argument('dataproperty',None)
+        limit = self.get_argument('limit',None)
+        full_definition = False
+        public = True if self.get_argument('public','0') == '1' else False
+       
+        entity = db.Entity(user_locale=self.get_user_locale())
+        
+        result = entity.get(ids_only=ids_only,entity_id=entity_id,search=keywords,
+                            entity_definition_keyname=entity_definition,dataproperty=dataproperty,limit=limit,
+                            full_definition=full_definition,only_public=public)
+        
+        if result == None:
+            self.write('')
+        elif size(result) > 5:
+            self.write(json.dumps(result))
+        else:
+            entity_ids = []
+            for entity in result:
+                entity_ids.append(entity['id'])
+            self.write(json.dumps(entity_ids))
+ 
+        
+        
+class View (myRequestHandler):
     """
     API open entity function.
     Needs entity ID.
@@ -35,7 +66,7 @@ class Add (myRequestHandler):
         entity_id = entity.create(entity_definition_keyname=self.get_argument('id'), parent_entity_id=('parent_id'))
         
         self.write("Add: 3. Done.\n")
-class Edit ():
+class Edit (myRequestHandler):
     """
     API edit entity function.
     
@@ -47,5 +78,7 @@ class Edit ():
         
 handlers = [
     ('/add', Add),
-    ('/open', Open),
+    ('/view', View),
+    ('/search', Search),
+    ('/edit', Edit),
 ]
