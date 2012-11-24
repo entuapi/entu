60b5e1c990220c624729e44f5e732c682b230a37
julia-gritsuk <julia.gritsuk@gmail.com>
2012-11-24 06:47:04 -0500

    edited the principle of giving arguments #57

--- tests/unittests/save_entity_test.py
+++ tests/unittests/save_entity_test.py
@@ -1,5 +1,6 @@
 import unittest
 import urllib
+import sys
 from sqlalchemy import *
 
 """
@@ -9,6 +10,8 @@ python save_entity_test.py ['entity_definition_keyname'] ['parent_id']
 
 ! Where ['entity_definition_keyname'] is mandatory and ['parent_id'] is optional
 """
+definition=None
+prentId=None
 
 def getLastId():
         db = create_engine('mysql://root:1234@localhost/entudb', echo=False)
@@ -23,25 +26,30 @@ def getLastId():
     
 def getArgs():
         query_args = {}
-        query_args['entity_definition_keyname'] = getValue(1)
-        query_args['parent_id'] = getValue(2)
+        query_args['entity_definition_keyname'] = definition
+        query_args['parent_id'] = parentId
         return query_args
 
 def getValue(argIndex):
         if argIndex < len(sys.argv):
-                return sys.argv(argIndex)
+            return sys.argv[argIndex]
         else:
-                return None
+            return None
         
 class TestSaveEntity(unittest.TestCase):
     def setUp(self):
         self.url = 'http://ec2-54-247-135-135.eu-west-1.compute.amazonaws.com:3307/save_entity'       
         self.encoded_args = urllib.urlencode(getArgs())
         lastId = getLastId()
-        self.expected_response = '{"entity_id": %s}' % (lastId+1)                
+        self.expected_response = '{"entity_id": %s}' % (lastId + 1)                
     def test_saveEntity(self):
         actual_response = urllib.urlopen("%s?%s" % (self.url, self.encoded_args)).read()
         self.assertEqual(self.expected_response, actual_response)
 
 if __name__ == "__main__":
-    unittest.main()
+        definition=getValue(1)
+	parentId=getValue(2)
+	if not definition:
+		print 'Error: No entity definition keyname given!'
+	else:
+		unittest.main(argv=[sys.argv[0]])
